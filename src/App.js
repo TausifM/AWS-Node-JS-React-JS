a983bb171d59dd9189f307ae36bcc2d6e6f80d6e
TausifM <heytausifsheikh@gmail.com>
2023-03-24 15:38:07 +0530

    aws configured

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import Users from "./component/Users.component";
 
 function App() {
+  // aws s3 bucket created name as user-processing-system-1 and 
+  // aws cli install in windows then command aws configure
+  // then user we have to create in iam, then that user we have to provide all permission in access adviser tab.
+  // then create access id and access key access center like us-east-1 and output as json.
+  // then in terminal aws s3 sync build/ s3://BUCKET NAME 
+  // above command we have to run.
+  // then in hoisting url your react project is live.
   return (
     <div className="App">
       <Users />
